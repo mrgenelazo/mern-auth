f705fc768412bf1c8e6ed670a9cbc04499df4047
Dennis <mr.dennisgenelazo@gmail.com>
2023-09-09 13:57:23 +0800

    create and run the server

---
+++ api/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.ENV || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
