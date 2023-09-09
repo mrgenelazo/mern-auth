5f4561862e64361595be89802b5dc477bbaa2850
Dennis <mr.dennisgenelazo@gmail.com>
2023-09-09 14:36:17 +0800

    mongoDB connection

--- api/server.js
+++ api/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/connectDB.js";
 
 const app = express();
 const port = process.env.ENV || 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+startServer();
